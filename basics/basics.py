60d970d571b3cc5177d5a53b0a470f5c03d45ffc
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-18 02:18:39 +0000

    basics of python

---
+++ basics/basics.py
@@ -0,0 +1,52 @@
+#basics python programs
+
+#assinging the values to variables
+#1st example
+a=1
+b=2
+c=3
+print(a,b,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print()#this will give space between two programs
+#2nd example
+a=10
+b=20
+c=a+b
+d=a-b
+e=a*b
+f=a//b
+print(c)
+print(d)
+print(e)
+print(f)
+
+print()
+
+#3rd example
+a=input("enter a number")#by defalut it will be in string when we take input
+b=input("enter a number")
+print(type(a))
+print(type(b))
+c=int(input("enter a number"))
+d=float(input("enter a number"))
+print(type(c))
+print(type(d))
+
+print()
+print()
+#4th example
+#converting into data types
+a=10
+print(a)
+print(type(a))
+a=float(a)
+print(type(a))
+b=11.25
+print(b)
+print(type(b))
+b=int(b)
+print(type(b))
+
