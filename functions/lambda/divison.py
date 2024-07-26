d71d3801436a3e6c343ba7697e125e5915372950
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-26 07:53:17 +0000

    lambda functions

---
+++ functions/lambda/divison.py
@@ -0,0 +1,7 @@
+#lambda functions
+#division
+t=lambda x,y : x//y
+x=30
+y=3
+print(t(x,y))
+
---
+++ functions/lambda/product.py
@@ -0,0 +1,7 @@
+#lambda functions
+#product
+t=lambda x,y: x*y
+x=int(input("enter the x value:"))
+y=int(input("enter the y value:"))
+print(t(x,y))
+
