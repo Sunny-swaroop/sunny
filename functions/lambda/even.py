a507d2f09ae468108e00d834267b750e5010f7c5
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-07-26 07:50:17 +0000

    lambda function

---
+++ functions/lambda/even.py
@@ -0,0 +1,6 @@
+#lambda functions
+#even or odd
+t=lambda n: "even" if n%2==0 else "odd"
+n=int(input("enter the n number:"))
+print(t(n))
+
---
+++ functions/lambda/sum.py
@@ -0,0 +1,7 @@
+#lambda functions
+#sum of 2 numbers
+t=lambda x,y :x+y
+x=int(input("enter the x value:"))
+y=int(input("enter the y value:"))
+print(t(x,y))
+
